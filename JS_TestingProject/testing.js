ac840522cfc355c2f84368ee73f2242b7af678cf
Pei <stevenkaoo@gmail.com>
2019-12-20 23:52:21 +1100

    js practice project

---
+++ JS_TestingProject/testing.js
@@ -0,0 +1,78 @@
+/*var firstName = prompt("Please enter your first name.");
+var lastName = prompt("Please enter your last name.");
+var age = prompt("Please enter your age.");
+
+alert("User's name is " + firstName + " " + lastName + ".");
+alert("User's age is " + age  + " years old.");
+//alert("This is an alert message.");
+//console.log("This is a testing message.");
+*/
+
+const nonStopFlights = [{
+  departure: 'Melbourne',
+  arrival: 'Beijing' 
+}];
+
+const oneStopFlights = [{
+  departure: 'Melbourne',
+  arrival: 'Guangzhou' 
+}, {
+  departure: 'Guangzhou',
+  arrival: 'Beijing' 
+}];
+
+const twoStopsFlights = [{
+  departure: 'Melbourne',
+  arrival: 'Singapore' 
+}, {
+  departure: 'Singapore',
+  arrival: 'Guangzhou' 
+}, {
+  departure: 'Guangzhou',
+  arrival: 'Beijing'
+}];
+
+const threeStopsFlights = [{
+  departure: 'Melbourne',
+  arrival: 'Sydney' 
+}, {
+  departure: 'Sydney',
+  arrival: 'Singapore'
+
+}, {
+  departure: 'Singapore',
+  arrival: 'Guangzhou' 
+}, {
+  departure: 'Guangzhou',
+  arrival: 'Beijing'
+}];
+
+function getStops(flights) {
+  const stopsMap = {
+    1: 'non-stop',
+    2: '1 stop',
+    4: 'day dream airline'
+  };
+  
+  var result = stopsMap[flights.length] || ((flights.length - 1) + ' stops');
+  console.log(result);
+};
+/*
+for(var i = -10; i < 20; i++) {
+  console.log(i);
+}
+
+var i = 10;
+while (i <= 41) {
+  if(i % 2 == 0) {
+    console.log(i);
+  }
+  i++;
+}
+*/
+var answer = prompt("Are we there yet?");
+while (answer.indexOf("yes") === -1) {
+  answer = prompt("Are we there yet?");
+} 
+  alert("YAY, WE MADE IT!.");
+;
