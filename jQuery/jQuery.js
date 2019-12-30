8b87d411baa477c57cf0e6226c2bef1c283251e1
Pei <stevenkaoo@gmail.com>
2019-12-30 18:15:08 +1100

    jQuery effect

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -5,3 +5,27 @@ $("div").css("background","purple")
 $("div:first-of-type").css("color","pink")
 $("img").attr("src","https://i.ytimg.com/vi/zx0gXM_FkgE/maxresdefault.jpg")
 $("select").val()
+
+$("button").click(function () { 
+  var text = $(this).text();
+  $(this).css("backgroundColor","pink");
+  console.log("You clicked " + text);
+});
+
+$("img").css("width", "200px");
+
+$("li").on("mouseenter", function() {
+  $(this).css("font-weight","bold");
+});
+
+$("li").on("mouseleave", function() {
+  $(this).css("font-weight","normal");
+});
+
+$("#click").on("click", function () {
+  /*$(".effect").fadeOut(300, function() {
+    $(this).remove();
+  });*/
+  $(".effect").slideToggle(500);
+});
+
